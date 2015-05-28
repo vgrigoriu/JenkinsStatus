b0a87401f7dd50083cf7c0ed8245bf76a12aba86
Victor Grigoriu <vgrigoriu@gmail.com>
2015-05-28 20:45:10 +0300

    Create custom JobList component

--- src/main.js
+++ src/main.js
@@ -1,4 +1,15 @@
+var JobList = React.createClass({
+    render: function () {
+        return (
+            <div class="job-list">
+                <div>Job 1</div>
+                <div>Job 2</div>
+            </div>
+        );
+    }
+});
+
 React.render(
-    <h1>cucu2</h1>,
+    <JobList />,
     document.getElementById('main')
 );
